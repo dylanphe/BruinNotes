ea5f7dead010d0144e71a639a82b7ecd2be6e099
dylanphe <dylanphe@g.ucla.edu>
2022-10-23 13:42:22 -0700

    Complete the frontend structure for acc_sys

---
+++ client/src/components/search_system/searchpage.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import {BsEyeFill, BsEyeSlashFill} from 'react-icons/bs';
+import "@fontsource/gloria-hallelujah";
+import './searchpage.css';
+
+// The function that toggles between themes
+function Searchpage() {
+
+    const navigate = useNavigate();
+    const [passwordShown, setPasswordShown] = useState(false);
+    // Password toggle handler
+    const togglePassword = () => {
+        // When the handler is invoked
+        // inverse the boolean state of passwordShown
+        setPasswordShown(!passwordShown);
+    };
+
+
+    return (
+        <div className='search-body'>
+            <span className= "search-btn-list">
+                <button className="search-btn" type="submit" onClick={()=>navigate("/")}>LOG OUT</button>
+            </span>
+            <p id="search-title-name">BruinNotes</p>
+        </div>
+    );
+}
+
+
+export default Searchpage;
