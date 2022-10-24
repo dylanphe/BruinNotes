2e3e4d8326067af910931c784a8353f80473f496
jackzhaotn <jackzhaotn@gmail.com>
2022-10-23 20:12:34 -0700

    [DOES NOT WORK] Add post request for addUser

--- client/src/components/account_system/signuppage.js
+++ client/src/components/account_system/signuppage.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect
 } from 'react';
 import "@fontsource/gloria-hallelujah";
 import './signuppage.css';
+import axios from 'axios'
 
 // The function that toggles between themes
 function SignupPage() {
@@ -12,10 +13,34 @@ function SignupPage() {
         console.log(data);
         return 0;
     }
+    asdf
 
-    useEffect(() => {
-        testConnection();
-    }, []);
+    const [firstname, setFirstname] = React.useState('')
+    const [lastname, setLastname] = React.useState('')
+    const [email, setEmail] = React.useState('')
+
+    const handleSubmit = (event) => {
+        const userInfo = {
+            'firstname': firstname,
+            'lastname': lastname,
+            'email': email
+        }
+
+        fetch("http://localhost:8000/adduser", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userInfo)
+        })
+    }
+    
+    // const addUserHandler = () => {
+    //     axios.post('http://127.0.0.1:8000/adduser', {'firstname': firstname, 'lastname': lastname, 'email': email})
+    //         .then(res => console.log(res))
+    // }
+
+    //useEffect(() => {
+    //    testConnection();
+    //}, []);
 
     return (
         <div className='signup-body'>
@@ -23,13 +48,13 @@ function SignupPage() {
             <button className="btn" id="signup-btn" type="submit">HOME</button>
             <div className='signup-box'>
                 <span id = "name-box">
-                    <input id='short-textbox'  type="text" placeholder="Enter Last Name"/> 
-                    <input id='short-textbox' type="text" placeholder="Enter First Name"/>
+                    <input onChange={event => setLastname(event.target.value)} id='short-textbox'  type="text" placeholder="Enter Last Name"/> 
+                    <input onChange={event => setFirstname(event.target.value)} id='short-textbox' type="text" placeholder="Enter First Name"/>
                 </span>
-                <input  id='long-textbox' type="text" placeholder="Enter UCLA Email Address"/>
+                <input  onChange={event => setEmail(event.target.value)} id='long-textbox' type="text" placeholder="Enter UCLA Email Address"/>
                 <input  id='long-textbox' type="text" placeholder="Enter Password"/>
                 <input  id='long-textbox' type="text" placeholder="Rytype Password"/>
-                <button className="btn" id="signup-btn" type="submit">SIGN UP</button>
+                <button  className="btn" id="signup-btn" type="submit" onClick={handleSubmit} >SIGN UP</button>
             </div>
         </div>
     );
